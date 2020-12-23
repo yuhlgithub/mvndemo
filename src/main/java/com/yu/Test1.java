0453e690f4ea68af95fedf1a123865cce851b48e
331772817@qq.com <331772817@qq.com>
2020-12-23 09:06:29 +0800

    modify

--- src/main/java/com/yu/Test1.java
+++ src/main/java/com/yu/Test1.java
@@ -1,10 +1,7 @@
 package com.yu;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.UUID;
+import java.io.*;
+import java.util.*;
 
 import org.junit.Test;
 
@@ -28,7 +25,7 @@ public class Test1 {
 
 
     @Test
-    public void test001() {
+    public void testInputStream() {
         File file = new File("/Users/hongluyu/work/local/docker.txt");
         // 1.创建流对象
         FileInputStream fis = null;
@@ -57,5 +54,103 @@ public class Test1 {
                 }
             }
         }
+    }
+
+
+    @Test
+    public void testOutputStream() {
+        // 1.选择流：创建流对象
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(new File("/Users/hongluyu/work/local/test.txt"), true);
+            // 2.准备数据源，把数据源转换成字节数组类型
+            String msg = "\n野径云俱黑，江船火独明。\n晓看红湿处，花丛锦官城。";
+            byte[] data = msg.getBytes();
+            // 3.通过流向文件当中写入数据
+            fos.write(data, 0, data.length);
+            // 4.刷新流
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                // 5.关闭流
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testSystemIn() {
+        int b;
+        try {
+            System.out.println("please Input:");
+            while ((b = System.in.read()) != -1) {
+                System.out.print((char) b);
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void test01(){
+        List<String> a = null;//new ArrayList();
+        for (String str: a){
+            System.out.printf("str");
+        }
+    }
+
+
+    @Test
+    public void test00(){
+        List<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(5);
+        al.add(2);
+        al.add(3);
+        al.add(100);
+        System.out.println(al.toString());
+
+        Collections.sort(al, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int i = o1-o2;
+                if (i>0){
+                    return 1;
+                }else if(i<0){
+                    return -1;
+                }
+                return 0;
+            }
+        });
+        System.out.println(al.toString());
+
+        al = al.subList(0,3);
+        System.out.println(al.toString());
+    }
+
+
+    @Test
+    public void test02(){
+        Date d1 = new Date();
+        Date d2 = new Date();
+        d2.setTime(d1.getTime() + 1000000);
+        System.out.println(d1.before(d2));
+    }
+
+
+
+    @Test
+    public void test03(){
+        String a = "1!2!3";
+        a = a.substring(0,a.lastIndexOf("!"));
+        System.out.println(a);
     }
 }
